5b8dbaf093acb5507869b026f35e5aa05efbf91c
sebazeballos <72205299+sebazeballos@users.noreply.github.com>
2022-09-23 13:41:56 -0300

    Detalle de producto

--- src/Components/listProducts/ItemListConteiner/Item/index.js
+++ src/Components/listProducts/ItemListConteiner/Item/index.js
@@ -7,15 +7,16 @@ import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 
 const Item = ({ product }) => {
-    console.log(product)
+    console.log(product.img)
     return(
         <>
         <ImageListItem key={product.id}>
           <img
-            src={`${product.img}?w=161&fit=crop&auto=format`}
-            srcSet={`${product.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
+            src={`${product.image}?w=161&fit=crop&auto=format`}
+            srcSet={`${product.image}?w=161&fit=crop&auto=format&dpr=2 2x`}
             alt={"producto"}
           />
+          
           <Typography variant="overline" size="15" display="block" gutterBottom>
             ${product.price}
           </Typography>
